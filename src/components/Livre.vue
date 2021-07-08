e773eb8b5b225e523d3eed5750d4acc009731b37
SamuelQuetin <samquetinuel@gmail.com>
2021-07-08 17:02:34 +0200

    ajout d'une liste de "livre"

---
+++ src/components/Livre.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="livre">
+    <h3>{{ livre.titre }}</h3>
+    <h4>{{ livre.auteur }}</h4>
+    <p>{{ livre.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    livre: {
+      type: Object,
+      required: true
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.livre {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.livre:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <livre v-for="livre in livres" :key="livre.id" :livre="livre"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Livre from "@/components/Livre.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Livre,
   },
+  data() {
+    return {
+      livres:
+          [
+            {
+              id: 1,
+              titre: 'titre',
+              auteur: 'auteur',
+              description: 'fdeafjalk elk jflakejmalk jlkfjaejk lahfjh a'
+            },
+            {
+              id: 2,
+              titre: 'titre2',
+              auteur: '2auteur',
+              description: 'fdeafj2alk elk jflakejmalk jlkfjaejk lahfjh a'
+            },
+            {
+              id: 3,
+              titre: 'ti3tre',
+              auteur: 'aute3ur',
+              description: 'fdea3fjalk elk jflakejmalk jlkfjaejk lahfjh a'
+            }
+          ]
+    }
+  }
+
 };
 </script>
